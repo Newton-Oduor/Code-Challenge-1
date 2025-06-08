1fb82316e2f34e29c85dd6ed7a12402abbafda1a
Newton Oduor <newtonochieng@gmail.com>
2025-06-08 23:58:35 +0300

    Code challenge chai

---
+++ chaiCalculator/chai.js
@@ -0,0 +1,28 @@
+function calculateChaiIngredients(numberOfCups) {
+
+    const waterPerCup = 200; // in ml
+    const milkPerCup = 50; // in ml
+    const majaniPerCup = 1; // in tablespoons
+    const sugarPerCup = 2; // in teaspoons
+
+    const totalWater = numberOfCups * waterPerCup;
+    const totalMilk = numberOfCups * milkPerCup;
+    const totalMajani = numberOfCups * majaniPerCup;
+    const totalSugar = numberOfCups * sugarPerCup;
+
+    console.log(`To make ${numberOfCups} cups of Kenyan Chai, you will need:`);
+    console.log(`Water: ${totalWater} ml`);
+    console.log(`Milk: ${totalMilk} ml`);
+    console.log(`Tea Leaves (Majani): ${totalMajani} tablespoon${totalMajani > 1 ? 's' : ''}`);
+    console.log(`Sugar (Sukari): ${totalSugar} teaspoon${totalSugar > 1 ? 's' : ''}`);
+    console.log('\nEnjoy your Chai Bora!');
+}
+
+const userInput = prompt("Karibu! How many cups of Chai Bora would you like to make?");
+const numberOfCups = parseInt(userInput);
+
+if (!isNaN(numberOfCups) && numberOfCups > 0) {
+  calculateChaiIngredients(numberOfCups);
+} else {
+  console.log("Please enter a valid number of cups.");
+}
